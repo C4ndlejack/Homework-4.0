fb72ed60f8deae88791290174ce879dbeef3f934
Candlejack <garouspeak@gmail.com>
2022-08-17 18:32:55 +0300

    exercise_2_is_done

---
+++ Exercise_2/exercise_2.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+using System;
+{
+    int[] toIntegerArray (int number)
+{
+    int a = number;
+    List<int> i = new List<int>();
+    while(a > 0)
+    {
+            i.Add(a % 10);
+            a = a / 10; 
+    }
+    return i.ToArray();
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] array = toIntegerArray(number);
+Console.WriteLine("Сумма цифр введенного числа: ");
+int j = 0;
+int result = 0;
+while(j < array.Length)
+{
+    result += array[j];
+    j++;
+}
+Console.WriteLine(result);
+}
