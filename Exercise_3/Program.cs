1a0b8a4f6483ada3abe3daddea1902929889b952
Candlejack <garouspeak@gmail.com>
2022-08-17 19:05:00 +0300

    exercise_3_is_done

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+using System;
+{
+    Console.WriteLine("Введите размер массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int [] array = new int [n];
+    Random rand = new Random();
+    Console.WriteLine("Результат: ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 50);
+        Console.Write(array[i] + " ");
+    }
+}    
